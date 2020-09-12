92b6d922d02759e243529451b0b0bb3e86ded056
Oladapo Bolarinwa <bolarinwaoladapo@gmail.com>
2020-09-12 21:28:34 +0100

    completed the seasons project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 class App extends React.Component{
-    render() {
+    state = {lat : null, errorMessage : ''};
+
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+            (position) => {
+                this.setState({lat: position.coords.latitude});
+            },
+            (err) => {
+                this.setState({errorMessage : err.message});
+            }
         );
-        return (
-            <div>Latitude: </div>
+    }
+
+    renderContent(){
+        if (this.state.lat && !this.state.errorMessage){
+            return (
+                <SeasonDisplay lat={this.state.lat} />
+            );
+        } 
+        else if (!this.state.lat && this.state.errorMessage){
+            return (
+                <div>Error: {this.state.errorMessage} </div>
+            );
+        }
+        else{
+            return (
+                <Spinner message='Please accept location request'/>
+            );
+        }
+    }
+
+    render() {
+        return(
+            <div className='border red'>
+                {this.renderContent()}
+            </div>
         );
     }
 }
